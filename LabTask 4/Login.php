570f40e3c6e1feeeec57534d8ca6dc30e7262323
Sriman-Dhar <dharsriman101@gmail.com>
2022-10-24 13:53:42 +0600

    Uploading

---
+++ LabTask 4/Login.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+	<title>Form Validation</title>
+</head>
+<body>
+		<form action="Validation.php" method="post" enctype="">
+		<fieldset>
+                        <img width = "40px" src="x.png" alt="">Company
+
+                </fieldset>
+
+                <fieldset>
+					<legend>LOGIN</legend>
+                    <table align = "center">
+					
+								<tr>
+									<td>User Name :</td>
+									<td><input type="text" name="UserName" value=""></td>
+
+								</tr>
+								
+								<tr>
+									<td>Password :</td>
+									<td><input type="password" name="password" value=""></td>
+
+								</tr>
+							
+								<tr>
+									<td><input type="checkbox">Remember me</td>
+								</tr>
+								
+								
+								<tr>
+									<td>
+										<input type="submit" name="" value="Submit">
+										<a href="Register.html"> Forgot Password? </a>
+									</td>
+								</tr>
+							
+
+                        </table>                    </fieldset>
+                    <fieldset>
+                    <p><center>Copyright c 2017</center></p>
+
+                    </fieldset>
+                </fieldset>
+			
+			</fieldset>
+		</form>
+</body>
+</html>
---
+++ LabTask 4/PublicHome.php
@@ -0,0 +1,25 @@
+<html>
+
+        <body>
+            
+                <fieldset>
+                        <img width = "40px" src="x.png" alt="">Company
+                        <table align = "right">
+                        <td><a href ="Registration.php">Home</a></td>
+                        <td><a href ="Registration.php">Login</a></td>
+                        <td><a href ="Registration.php">Registration</a></td>
+                        </table>
+                </fieldset>
+
+                <fieldset>
+                        <h4>Welcome to xCompany</h4>
+                    </fieldset>
+                    <fieldset>
+                    <p><center>Copyright c 2017</center></p>
+
+                    </fieldset>
+                </fieldset>
+        </body>
+
+</html>
+    
